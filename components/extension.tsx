4b8e2f5ae61da7b9c814b730484f8380d6dc6211
Haiderbukhari <apshaiderbukhari786@gmail.com>
2024-11-23 20:26:19 +0500

    initial setup

--- components/extension.tsx
+++ components/extension.tsx
@@ -2,7 +2,7 @@ const Extension = () => {
   return (
     <div className="flex flex-col w-full mb-20">
       <h1 className="text-3xl font-semibold">Extension</h1>
-      <button className="w-full h-10 px-3 py-2 text-xl rounded-md text-black" style={{ border: "1px solid #ccc" }}> Extension2</button>
+      {/* <button className="w-full h-10 px-3 py-2 text-xl rounded-md text-black" style={{ border: "1px solid #ccc" }}> Extension2</button> */}
     </div>
   )
 }
